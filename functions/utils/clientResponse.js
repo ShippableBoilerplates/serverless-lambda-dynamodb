04a658ab1ead5993dfbfda1330cec265517de20d
Lev E <bearnode@gmail.com>
2017-10-24 09:07:17 +0300

    fix response headers and implement public apikeys Signed-off-by: Lev E. <bearnode@gmail.com>

--- functions/utils/clientResponse.js
+++ functions/utils/clientResponse.js
@@ -1,6 +1,9 @@
 'use strict'
 exports.clientResponse = (statusCode, response) => {
     return {
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
         statusCode: statusCode,
         body: JSON.stringify(response)
     }
